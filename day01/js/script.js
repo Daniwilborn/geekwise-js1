84366acd414051d55c3114049eb6e5397fbbabf2
Daniwilborn <danimikay08@gmail.com>
2017-05-15 20:57:27 -0700

    day01

---
+++ day01/js/script.js
@@ -0,0 +1,59 @@
+// ES6 - ECMAScript6
+
+// Good
+var lowercase;
+var camelCase;
+var use_underscores;
+var _underscores;
+var $dollarsign;
+
+// Meh
+var SuperCamelCase;
+
+// Bad
+// var no spaces;
+// var no-hyphens;
+// var 1nonumber;
+
+console.log('Sending a message to the console');
+
+console.info('Sending an information message');
+
+console.warn('Here is a warning');
+
+console.error('Danger Will Robinson!')
+
+// console.log();
+
+// Single line comment
+
+/*
+Multi
+line
+Comments
+*/
+
+// White spaces
+var
+
+
+          freedom
+
+
+    =
+
+
+
+    "A string"+
+    "" +
+    "of Javascript";
+
+console.log(freedom);
+
+// Template string
+
+var javascriptTemplateString = '
+and
+    type
+    whatever
+    you want';
