5d7f26196aff2f516311bfd1529eb8e419d00046
Daniwilborn <danimikay08@gmail.com>
2017-05-24 21:02:07 -0700

    worked on day04

---
+++ day04/js/challenge.js
@@ -0,0 +1,16 @@
+// var firstName = prompt('what is your first name').trim();
+// console.log(firstName);
+// var firstLetter = firstName.charAt(0);
+// console.log(firstLetter);
+//  var firstLetterCap = firstLetter.toUpperCase();
+//  console.log(firstLetterCap);
+// var remainingLetters = firstName.substr(1);
+// console.log(remainingLetters);
+// var remainingLettersDrop = remainingLetters.toLowerCase();
+// console.log(remainingLettersDrop);
+// alert(firstLetterCap+remainingLettersDrop);
+//
+// var firstName = prompt('What is your first name?').trim();
+// firstName = firstName.charAt(0).toUpperCase() + firstName.substr(1).toLowerCase();
+// // alert(firstName);
+// console.log(firstName);
